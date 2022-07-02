b4121e2b573ea23456317e2d1c7abb32e58876ab
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-07-02 10:16:39 +0200

    2 files modified

--- src/app/gestione/gestione.component.ts
+++ src/app/gestione/gestione.component.ts
@@ -2,6 +2,42 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
 import { Spettacolo, Teatro } from '../app.component';
 
+export class nuovoSpettacolo extends Spettacolo {
+  nomeSpettacolo: string;
+  teatro: Teatro;
+  constructor(nomeSpettacolo) {
+    super();
+    this.nomeSpettacolo = nomeSpettacolo;
+  }
+  //genera un nuovo spettacolo vuoto con nome
+  genera(
+    filePlatea: number,
+    postiPlatea: number,
+    filePalco: number,
+    postipalco: number
+  ) {
+    this.teatro = new Teatro();
+    this.teatro.platea = Array(filePlatea)
+      .fill('fila')
+      .map(() =>
+        Array(postiPlatea)
+          .fill('posto')
+          .map((val, posto) => {
+            return undefined;
+          })
+      );
+    this.teatro.palco = Array(filePalco)
+      .fill('fila')
+      .map(() =>
+        Array(postipalco)
+          .fill('posto')
+          .map((val, posto) => {
+            return undefined;
+          })
+      );
+  }
+}
+
 @Component({
   selector: 'app-gestione',
   templateUrl: './gestione.component.html',
@@ -10,6 +46,7 @@ import { Spettacolo, Teatro } from '../app.component';
 export class GestioneComponent implements OnInit {
   @Input() spettacoli: Observable<Array<Spettacolo>>;
   @Output() spettacoliChange = new EventEmitter();
+  newSpettacolo: nuovoSpettacolo;
   teatro: Teatro;
   nomiSpettacoli: Array<string>;
   nomeSpettacolo: string;
@@ -24,14 +61,19 @@ export class GestioneComponent implements OnInit {
   constructor() {
     this.nomiSpettacoli = new Array();
     this.elemPlatea = new Array(10);
-    this.elemPalco;
+    this.elemPalco = new Array(10);
   }
-  seleziona() {
-    console.log(typeof this.filePlatea);
-    console.log(this.filePlatea);
-  }
-  inInput($event) {
-    console.log($event.target.value);
+  conferma() {
+    console.log([this.filePlatea, this.postiPlatea]);
+    console.log([this.filePalco, this.postiPalco]);
+    this.newSpettacolo = new nuovoSpettacolo(this.nomeSpettacolo);
+    this.newSpettacolo.genera(
+      this.filePlatea,
+      this.postiPlatea,
+      this.filePalco,
+      this.postiPalco
+    );
+    console.log(this.newSpettacolo);
   }
   vediSpettacoli() {
     this.showNomi = true;
